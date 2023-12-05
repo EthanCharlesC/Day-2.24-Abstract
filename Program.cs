9e02e05eb35a225de19f16151f9f64383499933f
Ethan Chinnasamy <ethan.chinnasamy@standardbank.co.za>
2023-12-05 14:03:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._24_Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Corsa car2 = new Corsa();
+            Console.WriteLine(car2.wheels);
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Vehicle 
+    {
+        public int wheels = 4;
+    }
+
+    class Corsa : Vehicle
+    {
+        public int spareWheels = 1;
+    }
+}
